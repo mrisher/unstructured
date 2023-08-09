308d48b8fd77d6ffa6d45770172233b79eab414c
potter-potter <david.potter@gmail.com>
2023-08-08 20:40:26 -0700

    initial commit

---
+++ unstructured/ingest/connector/salesforce.py
@@ -0,0 +1,17 @@
+import os
+import re
+from dataclasses import dataclass, field
+from pathlib import Path
+from typing import Type
+
+
+from unstructured.ingest.interfaces import (
+    BaseConnector,
+    BaseConnectorConfig,
+    BaseIngestDoc,
+    ConnectorCleanupMixin,
+    IngestDocCleanupMixin,
+    StandardConnectorConfig,
+)
+from unstructured.ingest.logger import logger
+from unstructured.utils import requires_dependencies
